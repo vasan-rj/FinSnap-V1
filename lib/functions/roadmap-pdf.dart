ec40fccb07a416bc1685341711259b6d2bdd879d
Vasan R <contact.vasan04@gmail.com>
2024-08-11 20:02:22 +0530

    err:after-roadmap+package-installation

--- lib/functions/roadmap-pdf.dart
+++ lib/functions/roadmap-pdf.dart
@@ -1,195 +1,89 @@
+import 'dart:io';
 import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw; 
-import 'package:printing/printing.dart'; 
- 
- Future<void> generateAndSavePDF() async {
-    final image = await imageFromAssetBundle('assets/logos/school-logo.png'); // import 'package:printing/printing.dart'
-    final doc = pw.Document(); // import 'package:pdf/widgets.dart' as pw
-    doc.addPage(
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+// import 'package:share_plus/share_plus.dart';
+
+
+// void sharePdf(String filePath) {
+//   Share.shareFiles([filePath], text: 'Here is your PDF!');
+// }
+
+
+
+Future<void> generatePdf(List<dynamic> phases) async {
+  final pdf = pw.Document();
+
+  try {
+    pdf.addPage(
       pw.Page(
-        pageFormat: PdfPageFormat.a4,
+      
         build: (pw.Context context) {
-          var widget;
-          return pw.Padding( // recreate the entire UI
-            padding: const pw.EdgeInsets.all(18.00),
-            child: pw.Column(
-              children: [
-                pw.Align(
-                  alignment: pw.Alignment.topCenter,
-                  child: pw.Image(image, width: 100, height: 100), // our school logo for the official PDF
-                ),
-                pw.Text(
-                  'Jackie & Co. Secondary School',
-                  style: const pw.TextStyle(fontSize: 17.00),
-                ),
-                pw.SizedBox(height: 10.00),
-                pw.Align(
-                  alignment: pw.Alignment.topLeft,
-                  child: pw.Row(
-                    mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-                    children: [
-                      pw.Text(
-                        'Name: ${widget.studentName}',
-                        style: const pw.TextStyle(fontSize: 15.00),
-                      ),
-                      pw.Text(
-                        'Form 4 West',
-                        style: const pw.TextStyle(fontSize: 15.00),
-                      ),
-                    ],
-                  ),
-                ),
-                pw.Divider(),
-                pw.SizedBox(height: 15.00),
-                pw.Row(
-                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+          return pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              pw.Text('Personalized Financial Roadmap',
+                  style: pw.TextStyle(
+                      fontSize: 24, fontWeight: pw.FontWeight.bold)),
+              pw.SizedBox(height: 20),
+              ...phases.map((phase) {
+                if (phase is! Map) return pw.Container(); // Skip if not a Map
+                
+                final phaseName = phase['phasename'] ?? 'Unknown Phase';
+                final duration = phase['duration'] ?? 'Unknown Duration';
+                final topics = phase['topics'] ?? [];
+                // final resources = phase['resource'] ?? [];
+
+                return pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
                   children: [
-                    pw.Column(
-                      crossAxisAlignment: pw.CrossAxisAlignment.start,
-                      children: [
-                        pw.Text(
-                          'Subject',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.SizedBox(height: 5.00),
-                        pw.Text(
-                          'Maths',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'English',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'Kiswahili',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'Physics',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'Biology',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'Chemistry',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'Geography',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          'Spanish',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                      ],
-                    ),
-                    pw.Column(
-                      crossAxisAlignment: pw.CrossAxisAlignment.start,
-                      children: [
-                        pw.Text(
-                          'Marks',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.SizedBox(height: 5.00),
-                        pw.Text(
-                          widget.studentResults['Maths'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['English'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Kiswahili'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Physics'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Biology'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Chemistry'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Geography'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Spanish'][0].toString(),
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                      ],
-                    ),
-                    pw.Column(
-                      crossAxisAlignment: pw.CrossAxisAlignment.start,
-                      children: [
-                        pw.Text(
-                          'Grade',
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.SizedBox(height: 5.00),
-                        pw.Text(
-                          widget.studentResults['Maths'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['English'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Kiswahili'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Physics'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Biology'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Chemistry'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Geography'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                        pw.Text(
-                          widget.studentResults['Spanish'][1],
-                          style: const pw.TextStyle(fontSize: 15.00),
-                        ),
-                      ],
-                    ),
+                    pw.Text(phaseName, style: pw.TextStyle(fontSize: 18, fontWeight: pw.FontWeight.bold)),
+                    pw.Text('Duration: $duration', style: pw.TextStyle(fontSize: 16)),
+                    pw.SizedBox(height: 10),
+                    pw.Text('Topics:', style: pw.TextStyle(fontSize: 16, fontWeight: pw.FontWeight.bold)),
+                    ...topics.map((topic) {
+                      if (topic is! Map) return pw.Container(); // Skip if not a Map
+                      
+                      final topicName = topic['topicname'] ?? 'Unknown Topic';
+                      final concepts = topic['concepts'] ?? [];
+
+                      return pw.Column(
+                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                        children: [
+                          pw.Text(topicName, style: pw.TextStyle(fontSize: 16, fontWeight: pw.FontWeight.bold)),
+                          ...concepts.map((concept) => pw.Text('- $concept', style: pw.TextStyle(fontSize: 14))),
+                        ],
+                      );
+                    }).toList(),
+                    pw.SizedBox(height: 10),
+                    // pw.Text('Resources:', style: pw.TextStyle(fontSize: 16, fontWeight: pw.FontWeight.bold)),
+                    // ...resources.map((resource) => pw.Text(resource, style: pw.TextStyle(fontSize: 14))),
+                    pw.SizedBox(height: 20),
+                    pw.Divider(),
                   ],
-                ),
-                pw.SizedBox(height: 30.00),
-                pw.Row(
-                  mainAxisAlignment: pw.MainAxisAlignment.center,
-                  children: [
-                    pw.Text(
-                      'Grand Total: ${widget.studentResults['Total'][0]}',
-                    ),
-                    pw.SizedBox(width: 15),
-                    pw.Text(
-                      'Mean Grade: ${widget.studentResults['Total'][1]}',
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                );
+              }).toList(),
+            ],
           );
         },
       ),
     );
-    await Printing.layoutPdf(onLayout: (PdfPageFormat format) async => doc.save());
+
+    // Save PDF file
+    final outputFile = await _getOutputFile();
+    final file = File(outputFile.path);
+    await file.writeAsBytes(await pdf.save());
+    print("\n"*5);
+    print('PDF saved to ${file.path}');
+    print("\n"*5);
+  } catch (e) {
+    print("Error generating PDF: $e");
   }
+}
+
+Future<File> _getOutputFile() async {
+  final directory = await getDownloadsDirectory();
+  final file = File('${directory?.path}/finsnap_financial_roadmap.pdf');
+  return file;
+}
+
